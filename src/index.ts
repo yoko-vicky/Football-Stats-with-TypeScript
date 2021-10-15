339a13e8eb9413ba4fca35876f0f3c5c7b3b76e0
yocosaka <sakayoco55@gmail.com>
2021-10-15 15:51:12 +0900

    Add simple flow like 1.LOAD & PARSE, 2.ANALYZE, 3. REPORT in a normal JavaScript syntax

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import fs from 'fs';
-const data = fs.readFileSync('../football.csv')
-console.log('data', data)
+
+// 1. LOAD & PARSE DATA
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8' // to return string, use utf-8
+})
+  .split('\n')
+  .map((row: string): string[] => row.split(','))
+  // row: [ '27/10/2018', 'Watford', 'Huddersfield', '3', '0', 'H', 'M Dean' ],
+// console.log(matches)
+
+// 2. ANALYZE DATA
+let manUnitedWins = 0;
+for (let match of matches) {
+  if ((match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')) {
+    manUnitedWins++
+  }
+}
+// 3. REPORT DATA
+console.log(`Report: Man United wins ${manUnitedWins} games!`)
