82d8ae07ecdc30711b081f7c556586a0cc308fba
yocosaka <sakayoco55@gmail.com>
2021-10-15 16:57:31 +0900

    Use enums to make the item of arrays more descriptive

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,34 @@ const matches = fs.readFileSync('football.csv', {
   // row: [ '27/10/2018', 'Watford', 'Huddersfield', '3', '0', 'H', 'M Dean' ],
 // console.log(matches)
 
-// 2. ANALYZE DATA
+
+
+// 2. ANALYZE DATAww
+
+// Enum - enumeration
+enum MatchResults { HomeWin = 'H', AwayWin = 'A', Draw = 'D' }
+
 let manUnitedWins = 0;
 for (let match of matches) {
-  if ((match[1] === 'Man United' && match[5] === 'H') ||
-    (match[2] === 'Man United' && match[5] === 'A')) {
+  if ((match[1] === 'Man United' && match[5] === MatchResults.HomeWin ) ||
+    (match[2] === 'Man United' && match[5] === MatchResults.AwayWin)) {
     manUnitedWins++
   }
 }
+
+
+// const matchResults = {
+//   homeWin: 'H',
+//   awayWin: 'A',
+//   draw: 'D'
+// }
+// const homeWin = 'H'
+// const awayWin = 'A'
+// const draw = 'D'
+
+
+
+
+
 // 3. REPORT DATA
 console.log(`Report: Man United wins ${manUnitedWins} games!`)
